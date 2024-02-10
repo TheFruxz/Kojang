2fe23f6238ed89b385ed51511eaf35aa6de62547
TheFruxz <cedricspitzer@outlook.de>
2024-02-10 14:23:31 +0100

    chore: update kotlin libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ dependencies {
 
     api("com.github.TheFruxz:Ascend:2023.3.4")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
-    implementation("io.ktor:ktor-client-cio:2.3.4")
-    implementation("io.ktor:ktor-client-core-jvm:2.3.4")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.8")
+    implementation("io.ktor:ktor-client-core-jvm:2.3.8")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.8")
 
     testImplementation(kotlin("test"))
 }
